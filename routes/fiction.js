7273a34823b3d3a0cae919f9b58b8ca8757b36be
Aude <aude.curial@gmail.com>
2025-10-28 18:07:32 +0100

    Import checkBody in fiction.js

--- routes/fiction.js
+++ routes/fiction.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 const { checkBody } = require('../modules/checkBody');
 
-// POST /fiction
+// POST /fiction -> CREATE A FICTION
+router.post('/', async (req, res) => {
+    try {
+    } catch (error) {
+        console.error('Error while fetching fictions:', error);
+        return res.status(500).json({ result: false, error: 'Internal server error' });
+    }
+});
 
 // GET /fiction -> RETRIEVE ALL FICTIONS
 router.get('/', async (req, res) => {
