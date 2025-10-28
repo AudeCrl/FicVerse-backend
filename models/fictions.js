e3ef6568088cf5464ef5ce299998d17e52435a1b
Aude <aude.curial@gmail.com>
2025-10-28 23:26:58 +0100

    Creation models/users.js and fictions.js

---
+++ models/fictions.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+// Values allowed for status
+const STORY_STATUS = ["in-progress", "completed", "one-shot", "abandoned"];
+const READING_STATUS = ["to-read", "reading", "finished"];
+
+const languageSchema = new mongoose.Schema({
+    name: { type: String, trim: true, required: true },
+    position: Number,
+});
+
+const fictionSchema = new mongoose.Schema({
+    title: { type: String, trim: true, required: true },
+    link: { type: String, trim: true },
+    summary: { type: String, trim: true },
+    personalNote: { type: String, trim: true },
+    author: { type: String, trim: true },
+    fandomId: { type: mongoose.Schema.Types.ObjectId, ref: "fandoms", required: true },
+    numberOfWords: { type: Number, min: 0 },
+    numberOfChapters: { type: Number, min: 0 },
+    readingStatus: { type: String, enum: READING_STATUS, required: true },
+    storyStatus: { type: String, enum: STORY_STATUS },
+    language: { type: languageSchema, required: false },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: Date,
+    image: { type: String, trim: true },
+    rate: {
+        value: { type: Number, min: 0, max: 5 },
+        display: { type: Boolean, default: false },
+    },
+    lastChapterRead: { type: Number, min: 0, default: 0 },
+});
+
+fictionSchema.index({ fandomId: 1 });
+
+// Search by author, or keyword in title
+fictionSchema.index({ author: 1 });
+fictionSchema.index({ title: 1 });
+
+// Search by keyword in either summary or personalNote
+fictionSchema.index(
+    { summary: "text", personalNote: "text"}, 
+    { name: "TextIndex_Content" }
+);
+
+// Filter (Tabs)
+fictionSchema.index({ readingStatus: 1 });
+
+// Sort
+fictionSchema.index({ createdAt: -1 });
+fictionSchema.index({ updatedAt: -1 });
+fictionSchema.index({ "rate.value": -1 });
+
+// To be confirmed ;-)
+fictionSchema.index({ "language.name": 1 });
+
+const Fiction = mongoose.model('Fiction', fictionSchema);
+
+module.exports = { Fiction };
--- models/users.js
+++ models/users.js
@@ -5,16 +5,16 @@ const NOTATION_ICON_TYPES = ['heart', 'star', 'flame','diamond'];
 const APPEARANCE_MODES = ["light", "dark", "system"];
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    avatarURL: String,
+    username: { type: String, trim: true, required: true, unique: true }, // Unique = automatic index
+    email: { type: String, trim: true, required: true, unique: true }, // Unique = automatic index
+    avatarURL: { type: String, trim: true },
     passwordHash: { type: String, required: true },
-    token: { type: String, index: true },
-    themeId: { type: mongoose.Schema.Types.ObjectId, ref: 'themes' },
+    token: { type: String, trim: true, unique: true }, // Unique = automatic index
+    themeId: { type: mongoose.Schema.Types.ObjectId, ref: 'themes' }, // plus tard : mettre par défaut l'ObjectId du thème watercolor une fois qu'il sera créé en base
     appearanceMode: { type: String, enum: APPEARANCE_MODES, default: 'system' },
     notationIcon: { type: String, enum: NOTATION_ICON_TYPES, default: 'heart' },
-    createdAt: Date,
-    lastConnectedAt: Date,
+    createdAt: { type: Date, default: Date.now },
+    lastConnectedAt: { type: Date, default: Date.now },
 });
 
 const User = mongoose.model('User', userSchema);
