9f817ba2c58cf43b373bf15f44e96659cb4c70ac
Aude <aude.curial@gmail.com>
2025-11-17 11:44:38 +0100

    Modif themeId -> theme + ENUM, variant system -> light by default

--- models/users.js
+++ models/users.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-// Values allowed to customize the notation icon and appearance mode.
+// Values allowed to customize the notation icon, theme and appearance mode.
 const NOTATION_ICON_TYPES = ['heart', 'star', 'flame','diamond'];
-const APPEARANCE_MODES = ["light", "dark", "system"];
+const THEMES = ['watercolor', 'ashgreen'];
+const APPEARANCE_MODES = ['light', 'dark'];
 
 const userSchema = new mongoose.Schema({
     username: { type: String, trim: true, required: true, unique: true }, // Unique = automatic index
@@ -10,8 +11,8 @@ const userSchema = new mongoose.Schema({
     avatarURL: { type: String, trim: true },
     passwordHash: { type: String, required: true },
     token: { type: String, trim: true, unique: true }, // Unique = automatic index
-    themeId: { type: mongoose.Schema.Types.ObjectId, ref: 'themes' }, // plus tard : mettre par défaut l'ObjectId du thème watercolor une fois qu'il sera créé en base
-    appearanceMode: { type: String, enum: APPEARANCE_MODES, default: 'system' },
+    theme: { type: String, enum: THEMES, default: 'watercolor' },
+    appearanceMode: { type: String, enum: APPEARANCE_MODES, default: 'light' },
     notationIcon: { type: String, enum: NOTATION_ICON_TYPES, default: 'heart' },
     lastConnectedAt: { type: Date, default: Date.now },
 },
